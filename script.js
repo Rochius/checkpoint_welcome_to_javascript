ce8ce378d0efbf023052436fc37dbb300df09ec9
Rocio Morales <99517293+Rochius@users.noreply.github.com>
2022-10-14 20:38:02 -0300

    :sparkles: Feat: Add call to the variables with console.log()

--- script.js
+++ script.js
@@ -1,33 +1,69 @@
 const userName = "Rocío";//type: String
+
 const userLastName = "Morales";//type: String
+
 const userBirthDate ={
     day: 25,//type: number
     month: 'October',//type: string
     year: 1993//type: number
 }//type: object
+
 var userAge = 28;//type: number
-const dni = "37497949";//type: string
+
 var phoneNumber = "381-5631-1536"//type: string
+
+var isSudent = true; //type: boolean
+
 var isMarried = false;//type: boolean
+
 var havePets = true;//type: boolean
-var address = "Calle Walabi 42 Sidney"; //type: String
-var favoriteMovies = ['El diario de Bridget Jones', 'Orgullo y Prejuicio', "El Orfanato"];//type: Array[String]
+
+var email = "rocio.mor.993@gmail.com"; //type: String
+
+var favoriteMovies = [
+    {
+        movieName: 'Bridget Jones Diary',
+        director: 'Sharon Maguire',
+        year: 2001
+    },
+    {
+        movieName: 'Pride and Prejudice',
+        director: 'Joe Wright',
+        year: 2005
+    },
+    {
+        movieName: 'El Orfanato',
+        director: 'Juan Antonio Bayona',
+        year: 2007
+    }
+];//type: Array[String]
+
 var favoriteTvShow = {
-    showName: 'Sailor Moon',//type: string
+    showName: 'Veronica Mars',//type: string
     start: {
-        day: 27,//type: number
-        month: 'March',//type: string
-        year: 1992//type: number
+        day: 22,//type: number
+        month: 'September',//type: string
+        year: 2004//type: number
     },
     end:{
-        day: 8,//type: number
-        month: 'February',//type: string
-        year: 1997//type: number
+        day: 19,//type: number
+        month: 'July',//type: string
+        year: 2019//type: number
     },
-    seasons:5,//type: number
-    episodes: 200//type: number
+    seasons:4,//type: number
+    episodes: 72//type: number
 }//type: Object
 
 
-console.log(`My name is ${userName} ${userLastName} and I have ${userAge} years old.`);
+console.log(`My name is ${userName} ${userLastName} and I'm ${userAge} years old.`);
 console.log(`Birth Date: ${userBirthDate.month} ${userBirthDate.day} of ${userBirthDate.year}`);
+console.log(`Telephone number: ${phoneNumber}`);
+console.log(`Have any pets?: ${havePets}`);
+console.log(`Is married?: ${isMarried}`);
+console.log(`Is student?: ${isSudent}`);
+console.log("Favorite TV Show:");
+console.log(`Show Name: ${favoriteTvShow.showName}; Start: ${favoriteTvShow.start.month} ${favoriteTvShow.start.year}; End: ${favoriteTvShow.end.month} ${favoriteTvShow.end.year}; Seasons: ${favoriteTvShow.seasons}; Episodes: ${favoriteTvShow.episodes}`)
+console.log('Favorite movies:')
+favoriteMovies.forEach(movie => {
+    console.log(`Movie name: ${movie.movieName}; Director: ${movie.director}; Year: ${movie.year}.`);
+})
